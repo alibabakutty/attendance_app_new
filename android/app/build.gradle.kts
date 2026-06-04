cdf9fc70a046d4b54cde3af00f227627e24c7e7f
alibabakutty <tallysriram007@gmail.com>
2026-06-04 10:27:22 +0530

    now package improved for apple development

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.attendance_app"
+    namespace = "com.kanishkaa.attendanceapp"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.attendance_app"
+        applicationId = "com.kanishkaa.attendanceapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
